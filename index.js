004dbb0a83b0c1bc700770202d945aafaa74497d
md-sarower <sarower9415@gmail.com>
2022-05-30 23:44:49 +0600

    real number added of two sister

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ app.get('/', (req, res) => {
 });
 
 const users = [
-    { id: 1, name: 'Ishrat Tabassum', email: 'ishrattabassum@gmail.com', phone: '01735112511' },
+    { id: 1, name: 'Ishrat Tabassum', email: 'ishrattabassum@gmail.com', phone: '01517992286' },
     { id: 2, name: 'Dipika Padkun', email: 'dipikapadkun@gmail.com', phone: '01735575648' },
     { id: 3, name: 'Priyanka Chopra', email: 'priyanka@gmail.com', phone: '01735574582' },
     { id: 4, name: 'Shek Jamal', email: 'shekjamal@gmail.com', phone: '01735745824' },
     { id: 5, name: 'Vobita Begum', email: 'vobita@gmail.com', phone: '01735115716' },
-    { id: 6, name: 'Nishat Tabassum', email: 'nishattabassum@gmail.com', phone: '01755111571' },
+    { id: 6, name: 'Nishat Tabassum', email: 'nishattabassum@gmail.com', phone: '01704757384' },
     { id: 7, name: 'Suraly Gomej', email: 'suralygomej@gmail.com', phone: '01758781111' },
 ]
 
